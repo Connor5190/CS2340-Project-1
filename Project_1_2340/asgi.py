2c0f79d514c785d01a6657223c481a9ee5f0de92
calebkeener13 <calebkeener13@gmail.com>
2024-09-11 15:43:45 -0400

    sample commit

---
+++ Project_1_2340/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Project_1_2340 project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Project_1_2340.settings')
+
+application = get_asgi_application()
