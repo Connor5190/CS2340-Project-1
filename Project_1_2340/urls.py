cd60c336ab308e97ca3fe2fff78e788b5145e859
IReyesH1 <ivanreyes.hernandez04@gmail.com>
2024-09-16 16:31:28 -0400

    I have created a test webpage that is linked to some html

--- Project_1_2340/urls.py
+++ Project_1_2340/urls.py
@@ -6,7 +6,7 @@ The `urlpatterns` list routes URLs to views. For more information please see:
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+    2. Add a URL to urlpatterns:  path('', views. home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
@@ -15,10 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
 from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('Resturant_Search/', include('Restaurant_Search.urls'))
+    path("", include("Restaurant_Search.urls"))
 ]
