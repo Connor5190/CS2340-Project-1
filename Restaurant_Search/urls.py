d8c8acd89037ee3cd9c201e87ca69d159145429f
calebkeener13 <calebkeener13@gmail.com>
2024-09-16 16:04:13 -0400

    This commit is creating the sign-up page in the case that the person trying to sign into the application has not already made an account with it.

--- Restaurant_Search/urls.py
+++ Restaurant_Search/urls.py
@@ -6,4 +6,5 @@ from Restaurant_Search import views
 urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('profile/', views.profile_view, name='profile'),
+    path('signup/', views.signup_view, name='signup'),
 ]
--- Restaurant_Search/views.py
+++ Restaurant_Search/views.py
@@ -5,6 +5,8 @@ from django.shortcuts import render
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 def login_view(request):
     if request.method == "POST":
@@ -21,3 +23,15 @@ def login_view(request):
 @login_required
 def profile_view(request):
     return render(request, "Restaurant_Search/profile.html", {'user': request.user})
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)  # 1
+        if form.is_valid():  # 2
+            user = form.save()  # 3
+            login(request, user)  # 4
+            messages.success(request, "Account created successfully!")  # 5
+            return redirect('profile')  # 6
+    else:
+        form = UserCreationForm()  # 7
+    return render(request, 'Restaurant_Search/signup.html', {'form': form})
