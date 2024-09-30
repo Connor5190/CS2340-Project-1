47e4c33807f4244353a06e89e7e36a103395601c
ryancapozzi <ryancapozzi3@gmail.com>
2024-09-29 23:04:25 -0400

    Added search feature and added search button

--- static/javascript/map_functionality.js
+++ static/javascript/map_functionality.js
@@ -2,6 +2,11 @@ let map;
 let latitude;
 let longitude;
 let marker;
+let service;
+let infowindow;
+let markers = []; // Array to hold markers
+let googleMapsLibs; // Global object to hold the libraries
+let isSearching = false; // Flag to prevent multiple searches
 
 window.onload = function() {
     if (navigator.geolocation) {
@@ -20,50 +25,101 @@ window.onload = function() {
     }
 };
 
-async function initMap() {
+// Function to load Google Maps libraries
+async function loadGoogleMapsLibraries() {
+    googleMapsLibs = {
+        Map: (await google.maps.importLibrary("maps")).Map,
+        InfoWindow: (await google.maps.importLibrary("maps")).InfoWindow,
+        Marker: (await google.maps.importLibrary("marker")).Marker,
+        PlacesService: (await google.maps.importLibrary("places")).PlacesService,
+    };
+}
 
-  // The location of the user
-  const position = { lat: latitude, lng: longitude};
-  // Request needed libraries.
-  //@ts-ignore
-  const { Map, InfoWindow } = await google.maps.importLibrary("maps");
-  const { AdvancedMarkerElement, PinElement } = await google.maps.importLibrary("marker");
-  const { PlacesService } = await google.maps.importLibrary("places")
+async function initMap(latitude, longitude) {
+    await loadGoogleMapsLibraries();
 
-  // The map, centered at user
-  map = new Map(document.getElementById("map"), {
-      zoom: 10,
-      center: position,
-      mapId: "86c19ff08dd83b77",
-      mapTypeControl: false,
+    const position = { lat: latitude, lng: longitude };
+
+    map = new googleMapsLibs.Map(document.getElementById("map"), {
+        zoom: 10,
+        center: position,
+        mapId: "DEMO_MAP_ID",
+        mapTypeControl: false,
     });
 
-  // The marker, positioned at your location
-  const pinTextGlyph = new PinElement({
-  glyph: "H",
-  glyphColor: "white",
-  });
+    marker = new googleMapsLibs.Marker({
+        position: position,
+        map: map,
+        title: 'User Location',
+    });
 
-  if (!marker) {
-      marker = new AdvancedMarkerElement({
-          map: map,
-          position: position,
-          content: pinTextGlyph.element,
-      });
-  } else {
-      marker.setPosition(position);
-  }
+    service = new googleMapsLibs.PlacesService(map);
+    infowindow = new googleMapsLibs.InfoWindow();
 
-  // Adding info windows with better information
-  const infoWindow = new InfoWindow({
-      content: "<p>Fill with details from Places API helper</p>",
-  });
+    const input = document.querySelector('.restaurant_search');
+    const searchButton = document.querySelector('.search_button');
 
-  marker.element.addEventListener("click", () => {
-      infoWindow.open({
-          anchor: marker,
-          map,
-          shouldFocus:false
-      });
-  });
+    searchButton.addEventListener('click', debounce(() => {
+        const query = input.value.trim(); // Get the user input for the search and trim whitespace
+        if (query) {
+            isSearching = true; // Set flag to indicate a search is in progress
+            performTextSearch(position, query); // Perform the search if the input is not empty
+            input.value = ''; // Clear the input after search
+            isSearching = false; // Reset the flag
+        } else if (!isSearching) {
+            alert("Please enter a search query."); // Alert if input is empty and not already searching
+        }
+    }, 300)); // 300ms delay to prevent rapid clicks
+}
+
+function debounce(func, delay) {
+    let timeout;
+    return function(...args) {
+        const context = this;
+        clearTimeout(timeout);
+        timeout = setTimeout(() => func.apply(context, args), delay);
+    };
+}
+
+function performTextSearch(position, query) {
+    const request = {
+        location: position,
+        radius: '500',
+        query: query
+    };
+
+    service.textSearch(request, callback);
+}
+
+function callback(results, status) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+        clearMarkers();
+
+        for (let i = 0; i < results.length; i++) {
+            const place = results[i];
+            createMarker(place);
+        }
+    }
+}
+
+function createMarker(place) {
+    const newMarker = new googleMapsLibs.Marker({
+        map: map,
+        position: place.geometry.location,
+        title: place.name,
+    });
+
+    markers.push(newMarker); // Add the new marker to the array
+
+    newMarker.addListener('click', () => {
+        infowindow.setContent(place.name);
+        infowindow.open(map, newMarker);
+    });
+}
+
+function clearMarkers() {
+    for (let i = 0; i < markers.length; i++) {
+        markers[i].setMap(null);
+    }
+    markers = []; // Clear the array after removing the markers
 }
